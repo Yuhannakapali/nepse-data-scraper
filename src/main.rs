30ae1f9dc0496108e56eb0195d5e0945dc44c65d
yuhanna <yuhanna.kapali@kotuko.it>
2022-03-09 10:22:34 +0545

    testing multiple intrative cli option

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
 // mod args;
+extern crate requestty;
+use requestty::{Question,prompt_one };
 mod nepse;
 use std::env;
 
 // use args::Args;
 
 fn main() {
-      let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
-        println!("you are breaking the law by scraping the Nepse Data.but if you still wanna continue then please provide the Nepse Symbol as the argument");
+        interactive_mode();
+    }else {
+        noninteractive_mode();
+        println!("I got {:?} arguments: {:?}.", args.len() - 1, &args[1..]);
     }
 
     
-    println!("I got {:?} arguments: {:?}.", args.len() - 1, &args[1..]);
+}
+
+
+fn interactive_mode() {
+    
+    let question = Question::expand("overwrite")
+    .message("Conflict on `file.rs`")
+    .choices(vec![
+        ('y', "Overwrite"),
+        ('a', "Overwrite this one and all next"),
+        ('d', "Show diff"),
+    ])
+    .default_separator()
+    .choice('x', "Abort")
+    .build();
+    let answer = prompt_one(question);
+println!("{:#?}",answer);
+    println!("interactive mode");
+}
+
+fn noninteractive_mode() {
+    println!("noninteractive mode");
 }
